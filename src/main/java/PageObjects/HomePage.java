0e26c4d4b15a7d65983cdb754ffe917159dc07d0
hnsandeep <hnsandeep94@gmail.com>
2023-07-06 23:03:27 +0200

    " Updated changes of locators and adding comments for functions  commit  "

--- src/main/java/PageObjects/HomePage.java
+++ src/main/java/PageObjects/HomePage.java
@@ -12,6 +12,10 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 import java.util.List;
+/*
+ * Author      : hnsandeep94
+ * Description : Singleton Guardian HomePageNews class to return single instance while passing driver object
+ * */
 
 public class HomePage {
     private WebDriver driver;
@@ -28,11 +32,10 @@ public class HomePage {
         }
         return homePage;
     }
-
     @FindBy(how = How.CSS, using = "[title ='Yes, I’m happy']")
     private static WebElement alertAcceptButton;
 
-    @FindBy(how = How.XPATH, using = "/html/body/div[3]/div/section[1]/div/div/div[1]/ul/li[1]/div/div/a/text()")
+    @FindBy(how = How.XPATH, using = "//*[@class='fc-slice-wrapper']//a[@data-link-name='article'][1]")
     private static WebElement getLatestNewsHeadline;
 
     @FindBy(how = How.XPATH, using = "//*[@class='fc-date-headline']")
@@ -41,7 +44,6 @@ public class HomePage {
     @FindBy(how = How.XPATH, using = "//*[@class='fc-item__title']//span[@class='js-headline-text'][1]")
     private WebElement headlIneObject;
 
-
     public void alert_Accept(WebDriver driver) {
         driver.switchTo().frame(driver.findElement(By.id("sp_message_iframe_801669")));
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
@@ -49,9 +51,12 @@ public class HomePage {
         alertAcceptButton.click();
     }
 
+    /*
+    * Author : hnsandeep94
+    * Description : function to get latestNews from guardian news page
+    * */
     public String getLatestnews(WebDriver driver) {
         String news = headlIneObject.getText();
-        // driver.findElement(By.xpath("//*[@class='fc-item__title']//span[@class='js-headline-text'][1]")).getText();
         return news;
     }
 }
--- src/test/java/StepDef/newsValidationTestSteps.java
+++ src/test/java/StepDef/newsValidationTestSteps.java
@@ -3,6 +3,7 @@ package StepDef;
 import PageObjects.GoogleSearchPage;
 import PageObjects.HomePage;
 import Utilities.TestBase;
+import io.cucumber.java.After;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -10,28 +11,39 @@ import io.cucumber.java.en.When;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
 import org.testng.asserts.Assertion;
+
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
+
+/*
+* Author : hnsandeep94
+* Descriptopm : Scenario to validate the news article on guardian site is not fake while comarin with google/other sorces
+* Parameters : Driver instance , PageClass singleton instances
+* */
+
 public class newsValidationTestSteps {
     private WebDriver driver;
-     private GoogleSearchPage googleSearchPage;
-     private HomePage homePage;
+    private GoogleSearchPage googleSearchPage;
+    private HomePage homePage;
 
     @Given("user launches the browser")
     public void user_launches_the_browser() throws IOException {
         driver = TestBase.setUp();
     }
+
     @When("user navigates to guardian site")
     public void user_navigates_to_guardian_site() throws IOException {
         driver.get(TestBase.prop.getProperty("baseurl"));
         homePage = HomePage.getInstance(driver);
         homePage.alert_Accept(driver);
     }
+
     @Then("user extract the details of the first news article")
-    public void user_extract_the_details_of_the_first_news_article()  {
+    public void user_extract_the_details_of_the_first_news_article() {
         try {
             String news = homePage.getLatestnews(driver);
             System.out.println("News obtained from article is " + news);
@@ -39,17 +51,14 @@ public class newsValidationTestSteps {
             googleSearchPage = GoogleSearchPage.getInstance(driver);
             googleSearchPage.alert_Accept(driver);
             boolean searchRes = false;
-            googleSearchPage.searchSubjectOnGoogle(driver,news);
+            googleSearchPage.searchSubjectOnGoogle(driver, news);
             List<WebElement> searchResults = googleSearchPage.getNewsSearcResults();
             for (WebElement element : searchResults) {
                 System.out.println("I am printing the news from searchengine = " + element.getText());
                 String newsONSearch = element.getText();
-                for( int i =0; i<news.length(); i++)
-                {
-                    for (int j = 0; j<newsONSearch.split(" ").length;  j++ )
-                    {
-                        if(news.contains(newsONSearch.split(" ")[j]))
-                        {
+                for (int i = 0; i < news.length(); i++) {
+                    for (int j = 0; j < newsONSearch.split(" ").length; j++) {
+                        if (news.contains(newsONSearch.split(" ")[j])) {
                             element.click();
                             searchRes = true;
                             break;
@@ -72,37 +81,26 @@ public class newsValidationTestSteps {
 
     }
 
-    @Given("user  lands on the news validation news site")
-    public void userLandsOnTheNewsValidationNewsSite() {
-    }
-
-    @When("user navigate to {string}")
-    public void user_navigate_to(String string) {
-
-    }
-
     @Then("user search for similar articles on Google")
     public void user_search_for_similar_articles_on_google() {
-
 
     }
 
     @Then("user validate that at least two articles are found")
-    public void user_validate_that_at_least_two_articles_are_found() {
-    }
-
+    public void user_validate_that_at_least_two_articles_are_found() {}
     @Then("user confirm that the first news article is valid")
-    public void user_confirm_that_the_first_news_article_is_valid() {
+    public void user_confirm_that_the_first_news_article_is_valid() {}
+    @When("user navigate to {string}")
+    public void user_navigate_to(String string) {}
 
+    @After
+    public void tearDown() {
+        // Close the browser and release resources
+        if (driver != null) {
+            driver.quit();
+        }
     }
 
-    @Then("user validates that no articles are found on google source")
-    public void userValidatesThatNoArticlesAreFoundOnGoogleSource() {
-
-    }
-
-    @And("user considers the first news article as invalid")
-    public void userConsidersTheFirstNewsArticleAsInvalid() {
-
-    }
 }
+
+
